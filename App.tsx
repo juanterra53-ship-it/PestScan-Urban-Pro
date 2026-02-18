3fa7fba37ce787ef34a0c43b6c30298cfffbed67
juanterra53-ship-it <juan.terra53@gmail.com>
2026-02-17 22:37:26 -0300

    Update App.tsx

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Camera from "./Camera";
+
+export default function App() {
+  return (
+    <div>
+      <h1>PestScan Urban Pro</h1>
+      <Camera />
+    </div>
+  );
+}
