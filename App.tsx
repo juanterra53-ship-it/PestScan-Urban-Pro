77a5af70a67cba27e5c51f437a0999875c11fa43
juanterra53-ship-it <juan.terra53@gmail.com>
2026-02-19 23:30:16 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,5 @@
-import React from "react";
-import Camera from "./Camera";
+import React from 'react';
 
 export default function App() {
-  return (
-   <div style={{background:"red",color:"white"}}>
-TESTE VERCEL 2026
-</div>
-
-
+  return null;
+}
