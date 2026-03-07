b7500cdb62fd7d2caba6da4a18912099c9d40d35
juanterra53-ship-it <juan.terra53@gmail.com>
2026-03-07 08:46:36 -0300

    Update declarations.d.ts

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -5,5 +5,15 @@ declare module '*.tflite' {
   export default content;
 }
 
+interface ImportMetaEnv {
+  readonly VITE_GEMINI_API_KEY: string;
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare const tf: any;
 declare const tflite: any;
