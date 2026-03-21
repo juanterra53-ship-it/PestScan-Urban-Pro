289604e8a7cf6369afd89aee1b7df89a359b4f5c
juanterra53-ship-it <juan.terra53@gmail.com>
2026-03-21 11:56:08 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ import {
 import { registerSW } from 'virtual:pwa-register';
 import './index.css';
 import { supabase } from './supabaseClient';
-import PrivacyPolicy from './components/PrivacyPolicy';
+import PrivacyPolicy from './components/PrivacyPolicy.tsx';
 import { 
   analyzePestImage, 
   analyzePestByName, 
